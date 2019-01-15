992c36581e5ce396f9c4ea97e579746f38e009c7
dgoncalves17 <david17_dvd@hotmail.com>
2019-01-15 14:40:37 +0000

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -2,11 +2,11 @@ import sqlite3
 from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
-
+test
 app = Flask (__name__)
 
 def open_connection();
-    connection = getattr(G, '_connection', None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
